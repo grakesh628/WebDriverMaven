d898be3f6a952cdffe1ae664f0c2ab5e2099b949
Rakesh Gundu <grakesh628@gmail.com>
2021-05-21 15:32:00 +0530

    my first push of WebDriverMaven Project

---
+++ src/test/java/com/w2a/testcases/LoginTest.java
@@ -0,0 +1,31 @@
+package com.w2a.testcases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+	public static WebDriver driver;
+	
+	@BeforeSuite
+	public void setUp() {
+		System.setProperty("webdriver.gecko.driver", "C:\\Users\\rakes\\workspace\\WebDriverMaven\\src\\test\\resources\\executables\\geckodriver.exe");
+		driver = new FirefoxDriver();
+	}
+	
+	@Test
+	public void doLogin() {
+		driver.get("http://gmail.com");
+		driver.findElement(By.id("Email")).sendKeys("grakesh628@gmail.com");
+		driver.findElement(By.id("Passwd")).sendKeys("Vault12#");
+	}
+	
+	@AfterSuite
+	public void tearDown() {
+		driver.quit();
+	}
+}
